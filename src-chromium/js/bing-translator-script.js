8e036bf4d99c43b75b78f5fed7d891147ca4aef6
Florian COLLIN <florian.collin83@gmail.com>
2022-03-20 17:04:00 +0100

    Update javascript file

--- src-chromium/js/bing-translator-script.js
+++ src-chromium/js/bing-translator-script.js
@@ -2,10 +2,24 @@
    @date : 2022
    Bing Translator JavaScript file
    */
+// Fonctions
+function setChildTextNode(elementId, text) {
+    document.getElementById(elementId).innerText = text;
+}
+
+function accesDoc() {
+    setChildTextNode('spanExtName', chrome.i18n.getMessage("extName"));
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#extName');
+    accesDoc();
+});
+
+// Ajoute un lien
 let plusLinkBr = document.createElement("br");
 document.getElementById("tt_translatorHome").appendChild(plusLinkBr);
 
 let plusLink = document.createElement("div");
     plusLink.id = "tt_translatorPlusContent";
 document.getElementById("tt_translatorHome").appendChild(plusLink);
-document.getElementById("tt_translatorPlusContent").innerHTML = "<table><tr><td>Plus d'infos</td></tr></table>";
+document.getElementById("tt_translatorPlusContent").innerHTML = '<table><tr><td>Plus d\'infos - <div id="extName"><span id="spanExtName"></span></div></td></tr></table>';
