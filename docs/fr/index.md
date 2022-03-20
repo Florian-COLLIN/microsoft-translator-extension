7b3f0854de3b11c2b39530774c8902fe730111f0
Florian COLLIN <florian.collin83@gmail.com>
2022-03-20 19:25:59 +0100

    Update

--- docs/fr/index.md
+++ docs/fr/index.md
@@ -5,8 +5,8 @@ L'extension Microsoft Translator Plus, est une extension qui a été conçu afin
 L'extension est compatible avec tous les navigateurs basés Chromium (Google Chrome, Microsoft Edge, Opera), mais n'est pour le moment disponible sur aucun store.
 
 ## Documentation
-- [Comment installer](/microsoft-translator-extension/docs/fr/how-to-install)
-- [Comment utiliser](/microsoft-translator-extension/docs/fr/how-to-use)
+- [Comment installer](/microsoft-translator-extension/fr/docs/how-to-install)
+- [Comment utiliser](/microsoft-translator-extension/fr/docs/how-to-use)
 - [Comment mettre à jour](/microsoft-translator-extension/fr/docs/how-to-update)
 
 ## Fonctionnalités
--- src-chromium/js/bing-translator-script.js
+++ src-chromium/js/bing-translator-script.js
@@ -2,18 +2,11 @@
    @date : 2022
    Bing Translator JavaScript file
    */
+let version_name = "v1.1.1-alpha.2 (Build 13)"
 // Fonctions
 function setChildTextNode(elementId, text) {
     document.getElementById(elementId).innerText = text;
 }
-
-function accesDoc() {
-    setChildTextNode('spanExtName', chrome.i18n.getMessage("extName"));
-}
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#extName');
-    accesDoc();
-});
 
 // Ajoute un lien
 let plusLinkBr = document.createElement("br");
@@ -22,4 +15,4 @@ document.getElementById("tt_translatorHome").appendChild(plusLinkBr);
 let plusLink = document.createElement("div");
     plusLink.id = "tt_translatorPlusContent";
 document.getElementById("tt_translatorHome").appendChild(plusLink);
-document.getElementById("tt_translatorPlusContent").innerHTML = '<table><tr><td>Plus d\'infos - <div id="extName"><span id="spanExtName"></span></div></td></tr></table>';
+document.getElementById("tt_translatorPlusContent").innerHTML = '<table><tr><td>Microsoft Translator Plus - 2022 - '+version_name+'</td></tr></table>';
